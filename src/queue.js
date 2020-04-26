bd2c96ed456053b5b9fe148c643818f4e3dd845f
Victor Barragan <43120861+VbarraganP@users.noreply.github.com>
2020-04-25 21:07:34 -0500

    Estructuras

---
+++ src/queue.js
@@ -0,0 +1,41 @@
+class Queue{
+    constructor(){
+        this.items = {};
+        this.front = 0;
+        this.end = 0;
+    };
+  
+    enqueue(data){
+        this.items[this.end] = data;
+        this.end++;
+    };
+
+    dequeue(){
+        if (this.front === this.end){
+            return null;
+        };
+        const data = this.items[this.front];
+        this.front++;
+        return data;
+    };
+
+    getSize(){
+        return this.end - this.front;
+    };
+
+    isEmpty(){
+        if(this.getSize()=== 0){
+            return true;
+        }else{
+            return false;
+        };
+    };
+
+    peek(){
+        if(this.getSize === 0){
+            return null;
+        };
+        return this.items[this.front];
+    }
+
+}
---
+++ src/stack.js
@@ -0,0 +1,40 @@
+class Stack {
+    constructor() {
+        this.items = {};
+        this.top = 0;
+    };
+ 
+    push(data){
+        this.top++;
+        this.items[this.top]= data;
+    };
+
+    pop(){
+        let deletedData;
+        if(this.top){
+            deletedData= this.items[this.top];
+            delete this.items[this.top];
+            this.top--;
+            return deletedData;
+        };
+    };
+
+    getSize(){
+        return this.top;
+    };
+
+    isEmpty(){
+        if(!this.getSize()){
+            return true;
+        }else{
+            return false;
+        };
+    };
+
+    peek(){
+        if(this.isEmpty()){
+            return null;
+        };
+        return this.items[this.top];
+    };
+};
