2a34b92b95d1cb87857eb87172fff5178bbb01b0
Felipe <jrojasce@unal.edu.co>
2020-06-30 20:31:17 -0500

    Heap incorporado a servicios

--- servi-app/src/componentes/ListPost/ListPost.js
+++ servi-app/src/componentes/ListPost/ListPost.js
@@ -1,21 +1,34 @@
 import React, { Component } from "react";
 import PostSummary from "./PostSummary";
 import { Link } from "react-router-dom";
+import Heap from "../../Estructuras/Heap";
+
 
 const ListPosts = ({ posts,id }) => {
-  
+  let Ht = new Heap(10);
+  posts && posts.map((post) => {
+      if(id==post.servicio){
+          let user = "prueba Cambiar"
+          let tel = post.telefono
+          let correo = post.correo
+          let ciudad = post.ciudad
+          let descripcion = post.descripcion
+          let puntuacion = post.calificacion
+          Ht.Insert(user, tel, correo, ciudad, descripcion, puntuacion)
+      }
+    })
+  const posts1 = Ht.HeapSort()
   return (
     <div className="text-sm-center">
-      {posts && 
-        posts.map((post) => {
-          if(id==post.servicio){
+      {posts1 && 
+        posts1.map((post,key) => {
             return (
-              <Link to={""} key={""}>
+              <Link to={""} key={post.calificacion}>
                 <PostSummary post={post} />
               </Link>
             )
           }
-        })}
+        )}
     </div>
   );
 };
--- servi-app/src/componentes/ListPost/PostSummary.js
+++ servi-app/src/componentes/ListPost/PostSummary.js
@@ -4,8 +4,8 @@ const PostSummary = ({ post }) => {
   return (
     <div className="as">
       <div className="card">
-        <h5 className="card-title">{post.calificacion}</h5>
-        <h6 className="card-subtitle mb-2 text-muted">{post.servicio}</h6>
+        <h5 className="card-title">{post.puntuacion}</h5>
+        <h6 className="card-subtitle mb-2 text-muted">{post.username}</h6>
         <p className="card-text">{post.descripcion}</p>
       </div>
     </div>
